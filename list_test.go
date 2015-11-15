2ac56087d298f731f0c28e4501f52ebd2923f65f
ajlopez <webmaster@ajlopez.com>
2015-11-15 19:20:52 -0300

    List rest additional test

--- list_test.go
+++ list_test.go
@@ -2,7 +2,7 @@ package golin
 
 import "testing"
 
-func TestListFirst(t *testing.T) {
+func TestListFirstRest(t *testing.T) {
     var l = List{42, nil}
     
     if l.First() != 42 {
@@ -12,4 +12,35 @@ func TestListFirst(t *testing.T) {
     if l.Rest() != nil {
         t.Fatal("l.Rest() is not nil")
     }
-}
+}
+
+func TestListWithTwoElements(t *testing.T) {    var l = List{42, &List{1, nil}}    
+    if l.First() != 42 {
+        t.Fatal("l.First() is not 42")
+    }
+    
+    if l.Rest() == nil {
+        t.Fatal("l.Rest() is nil")
+    }
+    
+    if l.Rest().First() != 1 {
+        t.Fatal("l.Rest().First() is not 1")
+    }
+    
+    if l.Rest().Rest() != nil {
+        t.Fatal("l.Rest().Rest() is not null")
+    }
+}
+
+func TestListFirstIsNil(t *testing.T) {
+    var l = List{nil, nil}
+
+    if l.First() != nil {
+        t.Fatal("l.First() is not nil")
+    }
+
+    if l.Rest() != nil {
+        t.Fatal("l.Rest() is not nil")
+    }
+}
+
