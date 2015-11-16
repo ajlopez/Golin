2a290240b9cf0fe2f5eb6426d00a914335094068
ajlopez <webmaster@ajlopez.com>
2015-11-16 06:32:21 -0300

    Atom.String()

--- atom.go
+++ atom.go
@@ -8,7 +8,7 @@ func (a *Atom) Evaluate(env *Env) SExpr {
     return env.Find(a.name)
 }
 
-func (a *Atom) AsString() string {
+func (a *Atom) String() string {
     return string(a.name)
 }
 
--- atom_test.go
+++ atom_test.go
@@ -9,6 +9,16 @@ func TestEvaluateUndefinedAtom(t *testing.T) {
     var result = atom.Evaluate(env)
     
     if result != nil {
-        t.Fail()
+        t.Fatal("atom.Evaluate(env) is not nil")
+    }
+}
+
+func TestAtomString(t *testing.T) {
+    var atom = Atom{"foo"}
+    
+    var result = atom.String()
+    
+    if result != "foo" {
+        t.Fatal("atom.String() is not foo")
     }
 }
