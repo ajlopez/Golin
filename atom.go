1644bf404ec54e314709610b708b45a452268232
ajlopez <webmaster@ajlopez.com>
2020-08-05 14:09:21 -0300

    Atom receiver as pointer in Assoc

--- atom.go
+++ atom.go
@@ -8,7 +8,7 @@ func (a Atom) Evaluate(env *Env) SExpr {
     return env.Get(a.name)
 }
 
-func (a Atom) Assoc(env *Env, value SExpr) {
+func (a *Atom) Assoc(env *Env, value SExpr) {
     env.Set(a.name, value)
 }
 
